bd5362939492185fc6224a3cb46ae774a93c605a
danhthai3 <ndthai2001@gmail.com>
2023-11-14 16:33:25 +0700

    front end

--- src/component/Home.js
+++ src/component/Home.js
@@ -546,7 +546,7 @@ export default function Home() {
                             <div style={{marginTop: '30px'}} className="now-detail-restaurant clearfix">
                                 <div className="container">
                                     <div className="row px-xl-5">
-                                        <div className="col-lg-5 pb-5">
+                                        <div className="col-lg-6 pb-5">
                                             <div id="product-carousel" className="carousel slide" data-ride="carousel">
                                                 <div style={{
                                                     width: '480px',
@@ -562,7 +562,7 @@ export default function Home() {
                                             </div>
                                         </div>
 
-                                        <div className="col-lg-7 pb-5">
+                                        <div className="col-lg-6 pb-5">
                                             <h3 className="font-weight-semi-bold">{product.name}</h3>
                                             {/*link dẫn tới merchant, cần có cả id merchant để lấy dữ liệu. */}
                                             <Link>{merchant.name} - Shop Online</Link>
